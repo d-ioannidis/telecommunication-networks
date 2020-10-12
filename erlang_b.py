8f89cb900d09430beef6ab7591fd1a0740df5291
dmitris1821gr <dimitrismagkas2@yahoo.gr>
2020-10-12 21:36:00 +0300

    update

--- erlang_b.py
+++ erlang_b.py
@@ -1,15 +1,3 @@
-"""
-A program used to calculate the Grade of Service and Quality of Service of a Telecommunication Network with two given
-inputs; the number of service lines and traffic load respectively, inserted in the Erlang B recursive formula and as a
-result, it extracts the overall call blockage and performance, both expressed as a probability and then as a percentage.
-With the help of the pandas library, it is also able to create an MS Excel file named 'erlang.xlsx' which includes a
-table with four columns and a dynamic multitude of rows which can be defined by how many inputs are given.
-https://en.wikipedia.org/wiki/Erlang_(unit)#Erlang_B_formula
-
-This project is helpful for those who may have to check how successful a Telecommunication Network can be, by simply
-typing in two inputs.
-"""
-
 import pandas as pd
 import sys
 
@@ -78,10 +66,9 @@ worksheet = writer.sheets['Table']
 workbook = writer.book
 format1 = workbook.add_format()
 format1.set_align('center')
+format2 = workbook.add_format()
+format2.set_border()
 
-worksheet.set_column('A:A', 10, format1)
-worksheet.set_column('B:B', 10, format1)
-worksheet.set_column('C:C', 10, format1)
-worksheet.set_column('D:D', 10, format1)
+worksheet.set_column('A:D', 10, format1)
 
 writer.save()
